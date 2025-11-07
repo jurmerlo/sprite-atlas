2acec5a7d409ed91709e0cfc9ae28e885388d6fe
Jurien Meerlo <jurmerlo@protonmail.com>
2025-11-07 02:26:43 +0000

    fixed relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  base: '/sprite-atlas/',
   build: {
     outDir: 'docs',
     rollupOptions: {
